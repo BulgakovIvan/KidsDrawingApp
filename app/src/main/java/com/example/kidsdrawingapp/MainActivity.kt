5dddb2efeb2eb45514b4274a40207e43ff4dfd05
BulgakovIvan <bulgivan@mail.ru>
2021-04-27 14:11:48 +0300

    dialog_brush_size.xml; add showBrushSizeChooserDialog()

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kidsdrawingapp.databinding.ActivityMainBinding
+import com.example.kidsdrawingapp.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,10 +12,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
         binding.drawingView.setSizeForBrush(20f)
 
+        binding.ibBrush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        var bindingDialog = DialogBrushSizeBinding.inflate(layoutInflater)
+
+        brushDialog.setContentView(bindingDialog.root)
+        brushDialog.setTitle("Brush size:")
+
+        val smallBtn = bindingDialog.ibSmallBrush
+        smallBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10f)
+            brushDialog.dismiss()
+        }
+
+        val mediumBtn = bindingDialog.ibMediumBrush
+        mediumBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20f)
+            brushDialog.dismiss()
+        }
+
+        val largeBtn = bindingDialog.ibLargeBrush
+        largeBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30f)
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
