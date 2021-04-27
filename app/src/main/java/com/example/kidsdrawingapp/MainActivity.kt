27cad7d91569f59317a16133cc5f12050175e8f0
BulgakovIvan <bulgivan@mail.ru>
2021-04-27 16:34:33 +0300

    Add ll_paint_colors;

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.kidsdrawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import com.example.kidsdrawingapp.databinding.ActivityMainBinding
 import com.example.kidsdrawingapp.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.drawingView.setSizeForBrush(20f)
+
+        mImageButtonCurrentPaint = binding.llPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         binding.ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
